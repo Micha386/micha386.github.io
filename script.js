28c98caf460668f55d3f638d6d4e085b3f57908e
Micha386 <michalina.zielezinska@gmail.com>
2025-05-09 12:51:15 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-
 function vote(answer) {
-  const img = document.getElementById('mainSprite');
+  const img = document.getElementById('mainImage');
   const msg = document.getElementById('responseMessage');
+
   if (answer === 'tak') {
-    img.src = "usmiech.png";
-    msg.textContent = "Dziękujemy! Do zobaczenia na weselu!";
+    window.location.href = "szczegoly.html";
   } else {
-    img.src = "smutek.png";
+    img.src = "placzace_postacie.png"; // Podmień na właściwy plik
     msg.textContent = "Będziemy tęsknić 😢";
   }
+
+  // Zapisz do Google Sheets
+  fetch("https://script.google.com/macros/s/AKfycbxYm7NI0G_dII2JiA4NGFCyKXeOo4f9Kq1lZKMtG1wTN_CVzFcEQpe0WkvtkqT4hxQ0/exec
+", {
+    method: "POST",
+    body: JSON.stringify({ odpowiedz: answer }),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).catch(err => console.error("Błąd zapisu:", err));
 }
